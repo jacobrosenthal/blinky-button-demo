342c4658dc6d7e9c3a8a432317c58af1ee21b6a6
Jacob Rosenthal <jacobrosenthal@gmail.com>
2022-01-16 09:44:03 -0700

    cmsisdap reproducer

--- src/main.rs
+++ src/main.rs
@@ -24,15 +24,15 @@ fn main() -> ! {
     let _executor = EXECUTOR.put(embassy::executor::Executor::new());
     let p = embassy_nrf::init(Default::default());
 
-    let button = Input::new(p.P0_11, Pull::Up);
-    let mut led = Output::new(p.P0_13, Level::Low, OutputDrive::Standard);
+    let button = Input::new(p.P1_00, Pull::Up);
+    let mut led = Output::new(p.P0_23, Level::Low, OutputDrive::Standard);
 
     rprintln!("Blinky button demo starting");
     loop {
         if button.is_high().unwrap() {
-            led.set_high().unwrap();
+            led.set_high();
         } else {
-            led.set_low().unwrap();
+            led.set_low();
         }
     }
 }
